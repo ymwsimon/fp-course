d46f988378d6aad7791a28e5c8a3b862452a2a85
Simon <mayeung@student.42london.com>
2024-08-22 16:10:32 +0200

    finish Extend

--- src/Course/Extend.hs
+++ src/Course/Extend.hs
@@ -9,6 +9,7 @@ import Course.ExactlyOne
 import Course.List
 import Course.Optional
 import Course.Functor
+import Data.ByteString.Char8 (tail, drop)
 
 -- | All instances of the `Extend` type-class must satisfy one law. This law
 -- is not checked by the compiler. This law is given as:
@@ -33,8 +34,7 @@ instance Extend ExactlyOne where
     (ExactlyOne a -> b)
     -> ExactlyOne a
     -> ExactlyOne b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance ExactlyOne"
+  f <<= eoa = ExactlyOne $ f eoa
 
 -- | Implement the @Extend@ instance for @List@.
 --
@@ -51,8 +51,8 @@ instance Extend List where
     (List a -> b)
     -> List a
     -> List b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance List"
+  _ <<= Nil = Nil
+  f <<= la@(_ :. as) = (f la :. Nil) ++ (f <<= as)
 
 -- | Implement the @Extend@ instance for @Optional@.
 --
@@ -66,9 +66,8 @@ instance Extend Optional where
     (Optional a -> b)
     -> Optional a
     -> Optional b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance Optional"
-
+  f <<= a@(Full _) = Full $ f a
+  _ <<= _ = Empty
 -- | Duplicate the functor using extension.
 --
 -- >>> cojoin (ExactlyOne 7)
@@ -86,5 +85,4 @@ cojoin ::
   Extend k =>
   k a
   -> k (k a)
-cojoin =
-  error "todo: Course.Extend#cojoin"
+cojoin = (id <<=)
