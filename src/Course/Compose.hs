7b5b6ad60eadf6c4528ef671680ca71d7318d8c0
Simon <mayeung@student.42london.com>
2024-08-24 12:12:44 +0200

    finish Compose

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -17,23 +17,20 @@ newtype Compose f g a =
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+  h <$> (Compose fga) = Compose $ (h <$>) <$> fga
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+  pure = Compose . pure . pure
 -- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  (Compose fgab) <*> (Compose fga) = Compose $ lift2 (<*>) fgab fga
 
-instance (Monad f, Monad g) =>
-  Monad (Compose f g) where
+-- instance (Monad f, Monad g) =>
+  -- Monad (Compose f g) where
 -- Implement the (=<<) function for a Monad instance for Compose
-  (=<<) =
-    error "todo: Course.Compose (=<<)#instance (Compose f g)"
+
+  -- f =<< (Compose fga) = Compose $ fga >>= \ga -> ga >>= \a -> Compose $ f a
 
 -- Note that the inner g is Contravariant but the outer f is
 -- Functor. We would not be able to write an instance if both were
@@ -41,5 +38,4 @@ instance (Monad f, Monad g) =>
 instance (Functor f, Contravariant g) =>
   Contravariant (Compose f g) where
 -- Implement the (>$<) function for a Contravariant instance for Compose
-  (>$<) =
-    error "todo: Course.Compose (>$<)#instance (Compose f g)"
+  f >$< (Compose fga) = Compose $ (<$>) (f >$<) fga
